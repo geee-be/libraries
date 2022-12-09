6b08980d05a740ef65a05558bd9159b0ba19c4a4
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2022-12-09 22:04:10 +1300

    more exports

--- packages/@geee-be/api/src/index.ts
+++ packages/@geee-be/api/src/index.ts
@@ -1,5 +1,8 @@
 export * from './api.js';
 export * from './error.js';
 export * from './inputs.js';
+export * from './koa-service.js';
+export * from './monitoring-service.js';
+export * from './types.js';
 
 // intentionally left empty
