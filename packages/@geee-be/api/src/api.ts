db6669574e1f56777591e9d889425f84ea30dee7
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2023-03-05 20:02:01 +1300

    missing exports

--- packages/@geee-be/api/src/api.ts
+++ packages/@geee-be/api/src/api.ts
@@ -1,18 +1,6 @@
 import Router from '@koa/router';
 import type { ApiContext } from './types.js';
 
-export type GetHandler<P = unknown, Q = unknown, R = unknown> = (
-  params: P,
-  query: Q,
-  ctx: Router.RouterContext,
-) => Promise<R>;
-export type Handler<B, P = unknown, Q = unknown, R = unknown> = (
-  body: B,
-  params: P,
-  query: Q,
-  ctx: Router.RouterContext,
-) => Promise<R>;
-
 export abstract class Api<StateT = any, CustomT = ApiContext> extends Router<StateT, CustomT> {
   /**
    * Create API
--- packages/@geee-be/api/src/index.ts
+++ packages/@geee-be/api/src/index.ts
@@ -1,9 +1,16 @@
 export * from './api.js';
 export * from './authorization.js';
+export * from './endpoint.js';
 export * from './error.js';
+export * from './handler.js';
 export * from './inputs.js';
 export * from './koa-service.js';
+export * from './mongo.js';
 export * from './monitoring-service.js';
+export * from './request-context.js';
+export * from './service.js';
 export * from './types.js';
+export * from './ulid.js';
+export * from './util.js';
 
 // intentionally left empty
