5b1e28abb10ea3479f89959e064ef32e6a9d9667
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2024-03-08 08:33:25 +1300

    default color

--- packages/@geee-be/react-twui/src/tw-plugin/colors/default.ts
+++ packages/@geee-be/react-twui/src/tw-plugin/colors/default.ts
@@ -2,10 +2,10 @@ import type { ThemableColorScale } from './themable-colors.js';
 
 export const defaultColors: ThemableColorScale = {
   // base
-  DEFAULT: 'hsl(0 0% 50%)',
+  DEFAULT: ['hsl(0 0% 40%)', 'hsl(0 0% 60%)'],
 
   // soft
-  soft: ['hsl(0 0% 50% / 0.5)', 'hsl(0 0% 50% / 0.5)'],
+  soft: ['hsl(0 0% 60% / 0.5)', 'hsl(0 0% 40% / 0.5)'],
   'soft-fg': ['hsl(0 0% 25%)', 'hsl(0 0% 88%)'],
 
   // vivid
