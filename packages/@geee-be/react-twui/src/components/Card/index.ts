bb4967b404a7c99926b8be0d9348b3fb0ea0e4c1
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2024-03-08 09:50:45 +1300

    export card components

--- packages/@geee-be/react-twui/src/components/Card/index.ts
+++ packages/@geee-be/react-twui/src/components/Card/index.ts
@@ -1,2 +1,5 @@
 export { Card } from './Card.js';
 export type { CardProps } from './Card.js';
+export { CardContent } from './CardContent';
+export { CardFooter } from './CardFooter';
+export { CardHeader } from './CardHeader';
